2b9ad2f100ff09496a2d14ffd9b27222b90d2d34
Krishna Singh <krishnasingh.02oct@gmail.com>
2026-03-26 09:54:02 +0530

    Final commit

--- week1/day1.rb
+++ week1/day1.rb
@@ -1,5 +1,5 @@
 #1 Even or Odd
-puts "Enter the number:"
+puts "Enter the number for even or odd:"
 a= gets.chomp.to_i
 if a%2==0
   puts "The given number is even"
@@ -13,7 +13,7 @@ a=gets.chomp
 puts a.length
 
 #3 Reverse
-puts "Enter the number:"
+puts "Enter the number for reverse:"
 a=gets.chomp.to_i
 reversed=0
 while a > 0
@@ -57,5 +57,41 @@ end
 name="Krishna Singh"
 puts name.split
 
-#6 Maximum among 3
+#6 Reverse the string
+name="Krishna Singh"
+word=name.split
+reversed=word.reverse
 
+#7 Maximum among 3
+puts "Enter the first number"
+a=gets.chomp.to_i
+puts "Enter the second number"
+b=gets.chomp.to_i
+puts "Enter the third number"
+c=gets.chomp.to_i
+if a>b && a>c
+  puts "a is max #{a}"
+elsif b>c && b>a
+  puts "b is max #{b}"
+else
+  puts "c is max #{c}"
+end
+
+#8 Checking Palindrome in numbers
+puts "Enter the number for Palindrome:"
+num=gets.chomp
+if num.reverse==num
+  puts "Yes the number is Palindrome"
+else
+  puts "NO the number is not Palindrome"
+end
+
+#9 Capitalize the string
+puts "Enter the string:"
+str=gets.chomp
+puts str.capitalize
+
+#10 Upcase the string
+puts "Enter the string:"
+str=gets.chomp
+puts str.upcase
