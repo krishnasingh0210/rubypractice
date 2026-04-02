aaaf129c02544f03cf060da48aaa2e0bb7d122e3
Krishna Singh <krishnasingh.02oct@gmail.com>
2026-04-02 22:16:12 +0530

    changed  commit

--- week1/day3.rb
+++ week1/day3.rb
@@ -5,6 +5,16 @@ fact=1
 num.times do|i|
   fact*=(i+1)
 end
-puts fact
+puts "The factorial is #{fact}"
 
-#2
+#2 Largest in array
+arr=[1,2,3,4,5]
+largest=0
+arr.each do|i|
+  if i>largest
+    largest=i
+  end
+end
+puts "The largest element in array is #{largest}"
+
+#3
