e853de35000049b41f76bdf2b36498d1edaceb6d
krishnasingh0210 <krishnasingh.02oct@gmail.com>
2026-04-23 18:16:43 +0530

    Final1

--- practices/practice1.rb
+++ practices/practice1.rb
@@ -1,23 +1,40 @@
+module Bill
+    def billing
+        puts "Getting bill details"
+    end
+end
 class Product
-    module My_product
-     def  product_id
-      puts "product_id is 1234" 
-     end 
+    attr_accessor :name, :price
+    def initialize(name, price)
+        @name=name 
+        @price=price
     end
 end
 class Order
-    module My_order
-     def  order_id
-      puts "order_id is 5678" 
-     end 
+    include Bill
+    attr_accessor :product, :quantity
+    def initialize (product, quantity)
+        @product=product
+        @quantity=quantity
+    end
+    def total_price
+        @product.price * @quantity
     end
 end
-class Payment
-    include Product::My_product
-    include Order::My_order
-    
+class Payment 
+    include Bill
+    attr_accessor :order, :payment_method
+    def initialize (order, payment_method)
+        @order=order
+        @payment_method=payment_method
+    end
+  def pay
+    puts "Processing payment of #{order.total_price} using #{payment_method}"
+  end
 end
-t = Payment.new
-t.product_id
-t.order_id
-puts "Order is confirmed"
+p=Product.new("Laptop", 1000)
+o=Order.new(p, 2)
+payment=Payment.new(o, "Credit Card")
+puts o.total_price
+puts payment.pay
+puts payment.billing
